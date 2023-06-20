ccc33f62cc9098fc0229fb40a355fbd7b7cde575
claydson <claydson.zip@gmail.com>
2023-06-20 10:49:21 -0300

    Segundo commit, adicionando classe scanner para receber dados do usuario

--- src/academy/devdojo/maratonajava/introducao/HelloWorld.java
+++ src/academy/devdojo/maratonajava/introducao/HelloWorld.java
@@ -1,8 +1,18 @@
 package academy.devdojo.maratonajava.introducao;
 
+import java.util.Scanner;
+
 public class HelloWorld {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Hello World");
+
+
+        System.out.println("Qual o seu nome ? ");
+        String nome = scanner.nextLine();
+
+        System.out.println("Bem-vindo ao meu primeiro programa " + nome);
+
+
         }
     }
