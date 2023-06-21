afd4b1cb71ccb3b581e9cb65a88b0b41ab87146d
claydson <claydson.zip@gmail.com>
2023-06-21 16:13:56 -0300

    Segunda aula tipos primitivos

--- src/academy/devdojo/maratonajava/introducao/Aula03TiposPrimitivosExercicio.java
+++ src/academy/devdojo/maratonajava/introducao/Aula03TiposPrimitivosExercicio.java
@@ -7,9 +7,10 @@ package academy.devdojo.maratonajava.introducao;
 * Eu <nome>, morando no endereçe <endereço>
     confirmo que recebi o salario de <salario>, na data <data>
 * */
+
 public class Aula03TiposPrimitivosExercicio {
     public static void main (String[] args){
-        String nome = "Claydson";
+        String nome = "Claydson Santana ";
         String endereco = "Rua Francisco Bento de Oliveira 78";
         double salario = 5000;
         String data = "27/05/2023";
