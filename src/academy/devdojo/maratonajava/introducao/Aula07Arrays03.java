2a5861e4ca087918bde9ff10e4c78ada78f87f0f
claydson <claydson.zip@gmail.com>
2023-06-27 19:50:03 -0300

    Array e foreach

---
+++ src/academy/devdojo/maratonajava/introducao/Aula07Arrays03.java
@@ -0,0 +1,13 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula07Arrays03 {
+    public static void main(String[] args) {
+        int [] numeros = {1, 2, 3, 4, 5};
+        int [] numeros2 = new int[] {1, 2, 3, 4, 5};
+
+        for (int i:numeros2) {
+            System.out.println(i);
+        }
+
+    }
+}
