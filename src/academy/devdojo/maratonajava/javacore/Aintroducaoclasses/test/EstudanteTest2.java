3e35b6cd1a0f00121101e0d1c201fb4040df73b3
claydson <claydson.zip@gmail.com>
2023-06-30 20:16:48 -0300

    Segunda aula de introdução a Orintação a Objetos

---
+++ src/academy/devdojo/maratonajava/javacore/Aintroducaoclasses/test/EstudanteTest2.java
@@ -0,0 +1,26 @@
+package academy.devdojo.maratonajava.javacore.Aintroducaoclasses.test;
+
+import academy.devdojo.maratonajava.javacore.Aintroducaoclasses.dominio.Estudante;
+
+public class EstudanteTest2 {
+    public static void main(String[] args) {
+        Estudante estudante = new Estudante();
+        Estudante estudante2 = new Estudante();
+
+        estudante.nome = "Wandra";
+        estudante.idade = 32;
+        estudante.sexo = 'F';
+
+        estudante2.nome = "Claydson";
+        estudante2.idade = 26;
+        estudante2.sexo = 'M';
+
+        System.out.println(estudante.nome);
+        System.out.println(estudante.idade);
+        System.out.println(estudante.sexo);
+        System.out.println("-----------------");
+        System.out.println(estudante2.nome);
+        System.out.println(estudante2.idade);
+        System.out.println(estudante2.sexo);
+    }
+}
